cd7b3b101cb9720d9fdd4d25ea4eb3a8345c9bef
minhthao86ppk-commits <minhthao86ppk@gmail.com>
2026-08-20 07:33:45 +0700

    Update AppDelegate.swift

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -3,49 +3,37 @@ import Capacitor
 import CoreLocation
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate {
+class AppDelegate: CAPBridgeAppDelegate, CLLocationManagerDelegate {
 
-    var window: UIWindow?
     var locationManager: CLLocationManager?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        // Khởi tạo GPS Native trong hàm chính để không bị lỗi build
+        // 1. Khởi tạo bộ định vị Native
         self.locationManager = CLLocationManager()
         self.locationManager?.delegate = self
         self.locationManager?.desiredAccuracy = kCLLocationAccuracyBestForNavigation
         self.locationManager?.distanceFilter = 2
         
-        // Cấu hình chạy ngầm không ngắt quãng
+        // 2. Cấu hình chạy ngầm liên tục khi khóa màn hình
         self.locationManager?.pausesLocationUpdatesAutomatically = false
         self.locationManager?.allowsBackgroundLocationUpdates = true
         self.locationManager?.showsBackgroundLocationIndicator = true
-        
         self.locationManager?.requestAlwaysAuthorization()
         self.locationManager?.startUpdatingLocation()
 
-        return true
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        // Duy trì tiến trình chạy ngầm khi khóa màn hình
+        // Tiến trình nhận GPS ngầm được duy trì liên tục
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {}
-
-    func applicationDidEnterBackground(_ application: UIApplication) {}
-
-    func applicationWillEnterForeground(_ application: UIApplication) {}
-
-    func applicationDidBecomeActive(_ application: UIApplication) {}
-
-    func applicationWillTerminate(_ application: UIApplication) {}
-
-    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+    override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
         return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
     }
 
-    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+    override func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
         return ApplicationDelegateProxy.shared.application(application, continue: userActivity, restorationHandler: restorationHandler)
     }
 }
